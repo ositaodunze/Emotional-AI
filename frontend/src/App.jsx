d6435d467ab782d394fe651406ba5192f070ba13
Jaunel Panton <pantonjaunel@gmail.com>
2025-10-14 19:46:50 -0400

    Detection using face-api.js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import CamFeed from "./components/CamFeed.jsx";
+
+function App() {
+  const [emotion, setEmotion] = useState("");
+
+  return (
+    <>
+      <div className="flex flex-col items-center p-6">
+        <h1 className="text-3xl font-bold mb-4 text-blue-700">
+          Emotion AI Music Player
+        </h1>
+        <CamFeed onEmotionDetected={setEmotion} />
+      </div>
+    </>
+  );
+}
+
+export default App
