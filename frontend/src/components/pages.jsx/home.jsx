b74fcb17527b4b595cf8f9faee8e6e9e96214bfc
NPotts24 <najaepretty@gmail.com>
2025-11-03 22:58:14 -0500

    git commit -m

--- frontend/src/components/pages.jsx/home.jsx
+++ frontend/src/components/pages.jsx/home.jsx
@@ -6,3 +6,4 @@ window.alert(`Your face is out of the frame`);
 
 document.getElementById("myH1").textContent = `Welcome to Feed Music AI`;
 document.getElementById("myP").textContent = `Place your face into the camera to scan for your mood`
+
