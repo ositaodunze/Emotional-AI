4a13b96092b9441268e70e4e2f442a03b0d8d32d
NPotts24 <najaepretty@gmail.com>
2025-11-03 22:47:23 -0500

    git commit -m "Updated home page style and content"

---
+++ frontend/src/components/pages.jsx/home.jsx
@@ -0,0 +1,8 @@
+console.log(`hello`);
+console.log(``);
+
+window.alert(`Your face is out of the frame`);
+//comment
+
+document.getElementById("myH1").textContent = `Welcome to Feed Music AI`;
+document.getElementById("myP").textContent = `Place your face into the camera to scan for your mood`
