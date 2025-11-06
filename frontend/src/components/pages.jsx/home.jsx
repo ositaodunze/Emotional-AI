4d3b46fc9082f8b70ea328a8fe9a3de9f4a9b04f
NPotts24 <najaepretty@gmail.com>
2025-11-06 12:26:09 -0500

    changed entire html doc for structure purposes

--- frontend/src/components/pages.jsx/home.jsx
+++ frontend/src/components/pages.jsx/home.jsx
@@ -3,13 +3,14 @@
 
 window.alert(`Your face is out of the frame`);
 //comment: add a conditional statement to check if face is in frame
-if (faceInFrame) {
+/*if (faceInFrame) {
     window.alert(`Your face is in the frame`);
 }
 
 else{
     window.alert(`Your face is out of the frame`);
 }
+*/
 
 document.getElementById("myH1").textContent = `Welcome to Feed Music AI`;
 document.getElementById("myP").textContent = `Place your face into the camera to scan for your mood`;
