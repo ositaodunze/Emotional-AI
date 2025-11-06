a0a22c5bab7b280492ff976995761ab25fe33fbf
NPotts24 <najaepretty@gmail.com>
2025-11-06 13:27:00 -0500

    changing instructions in the jsx file

--- frontend/src/components/pages.jsx/home.jsx
+++ frontend/src/components/pages.jsx/home.jsx
@@ -13,6 +13,6 @@ else{
 */
 
 document.getElementById("myH1").textContent = `Welcome to Feed Music AI`;
-document.getElementById("myP").textContent = `Place your face into the camera to scan for your mood`;
+document.getElementById("myP").textContent = `Click the button below to calibrate your mood`;
 
 
