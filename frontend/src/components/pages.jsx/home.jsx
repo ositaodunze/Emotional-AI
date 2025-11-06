71b3d86b736f55443dcc6007919216d53252727b
NPotts24 <najaepretty@gmail.com>
2025-11-05 22:43:26 -0500

    added a few comments

--- frontend/src/components/pages.jsx/home.jsx
+++ frontend/src/components/pages.jsx/home.jsx
@@ -2,7 +2,8 @@ console.log(`hello`);
 console.log(`goodbye`);
 
 window.alert(`Your face is out of the frame`);
-//comment
+//comment: add a conditional statement to check if face is in frame
+
 
 document.getElementById("myH1").textContent = `Welcome to Feed Music AI`;
 document.getElementById("myP").textContent = `Place your face into the camera to scan for your mood`
