c3f0d1c2022ed1a5a82af3ddc7332ca0399f6c2e
NPotts24 <najaepretty@gmail.com>
2025-11-05 23:22:00 -0500

    fixed syntax error in jsx code

--- frontend/src/components/pages.jsx/home.jsx
+++ frontend/src/components/pages.jsx/home.jsx
@@ -12,5 +12,5 @@ else{
 }
 
 document.getElementById("myH1").textContent = `Welcome to Feed Music AI`;
-document.getElementById("myP").textContent = `Place your face into the camera to scan for your mood`
+document.getElementById("myP").textContent = `Place your face into the camera to scan for your mood`;
 
