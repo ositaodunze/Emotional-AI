c92859356ae69669b5ee71212f9a05e09102350a
NPotts24 <najaepretty@gmail.com>
2025-11-06 00:38:45 -0500

    updating

--- frontend/src/components/pages.jsx/home.jsx
+++ frontend/src/components/pages.jsx/home.jsx
@@ -14,3 +14,4 @@ else{
 document.getElementById("myH1").textContent = `Welcome to Feed Music AI`;
 document.getElementById("myP").textContent = `Place your face into the camera to scan for your mood`;
 
+
