339a3927cc49354c15dc7216221a68a2bc41f34e
Jaunel Panton <pantonjaunel@gmail.com>
2025-11-13 11:45:44 -0500

    Created mapping model, updated requirements

--- backend/app/api.py
+++ backend/app/api.py
@@ -4,8 +4,11 @@ from fastapi.responses import RedirectResponse, JSONResponse
 from pydantic import BaseModel
 import spotify_recc
 import spotify_api
-import emotion_utils
 import json
+import requests #useful for recommendations from reccobeats
+import joblib
+import numpy as np 
+import tensorflow as tf
 
 
 app = FastAPI()
@@ -22,6 +25,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+RECCOBEATS_BASE = "https://api.reccobeats.com/v1/track/recommendation"
+
+#model
+ohe = joblib.load("models/mood_encoder.pkl")
+scaler_y = joblib.load("models/feature_scaler.pkl")
+ml_model = tf.keras.models.load_model("models/mood_to_features.h5")
 
 class EmotionInput(BaseModel):
     emotion: str
@@ -77,31 +87,84 @@ def get_user_genres(request: Request):
         print("Error fetching user genres:", e)
         return {"genres": []}
 
-    
+
+def predict_track_features(mood):
+    X = ohe.transform([[mood]])
+    pred_scaled = ml_model.predict(X)
+    features = scaler_y.inverse_transform(pred_scaled)[0] 
+    return features 
+
+def call_reccobeats(feature_params,limit=20):
+    payload ={
+        "limit":limit, "audio_features":feature_params
+    }
+    try:
+        response = requests.post(RECCOBEATS_BASE,json=payload,timeout=10)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print("ReccoBeats Error", e)
+        return None
+
+def convert_to_reccobeats(predicted_features,feature_names):
+    params = {}
+    for idx, col in enumerate(feature_names):
+        key = f"target_{col}"
+        params[key] = float(predicted_features[idx])
+    return params
+
 @app.post("/api/recommendation")
 async def get_recommendations(request: Request):
     body = await request.json()
     emotion = body.get("emotion")
     selected_genres = body.get("genres", [])
+    
     token_info_json = request.cookies.get("spotify_token_info")
     if not token_info_json:
         return JSONResponse({"error": "Not logged in"}, status_code=401)
+    
     sp = spotify_api.get_spotify_client(token_info_json)
-    emotion_query = emotion_utils.get_emotion_query(emotion)
-    combined_query = f"{emotion_query} {' '.join(selected_genres)}"
-    results = sp.search(q=combined_query, type="track", limit=10)
+    try:
+        top_artists = sp.current_user_top_artists(limit=10,time_range="medium_term")
+        seed_artists = [artist["id"] for artist in top_artists["items"][:5]]
+        user_genres = [genre for artist in top_artists["items"] for genre in artist["genres"]]
 
-    recommendations = [
-    {
-        "name": item["name"],
-        "artist": item["artists"][0]["name"],
-        "url": item["external_urls"]["spotify"],
-        "uri": item["uri"],
-    }
-    for item in results["tracks"]["items"]
-]
+    except Exception as e:
+        print("Failed to fetch top artists:", e)
+        seed_artists = []
+        user_genres = []
 
-    if not recommendations:
+    combined_genres = list(set(selected_genres + user_genres))
+
+    #tuning 
+    valence = 0.8 if "happy" in emotion else 0.3 if "sad" in emotion else 0.5
+    energy = 0.7 if "angry" in emotion else 0.4
+    danceability = 0.8 if "happy" in emotion else 0.5
+
+    try:
+        results = sp.recommendations(seed_artists=seed_artists[:3],
+            seed_genres=combined_genres[:2],
+            limit=10,
+            target_valence=valence,
+            target_energy=energy,
+            target_danceability=danceability,)
+        results = sp.search()
+
+        recommendations = [
+            {
+                "name": item["name"],
+                "artist": item["artists"][0]["name"],
+                "url": item["external_urls"]["spotify"],
+                "uri": item["uri"],
+            }
+            for item in results["tracks"]
+        ]
+        if not recommendations:
+            raise Exception("Empty recommendations")
+        return {"recommendations": recommendations}
+    
+    except Exception as e:
+        print("Error fetching recs",e)
         # Fallback: search a generic playlist if nothing is found
         fallback_results = sp.search(q="mood booster", type="track", limit=10)
         recommendations = [
@@ -113,10 +176,8 @@ async def get_recommendations(request: Request):
             }
             for item in fallback_results["tracks"]["items"]
         ]
-
-    return {"recommendations": recommendations}
-
-
+        return {"recommendations": recommendations}
+            
 @app.post("/api/play")
 async def play(request: Request):
     body = await request.json()
---
+++ backend/emotionmusic.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+import joblib
+
+df = pd.read_csv("data/dataset.csv")
+
+#Using Russell's Circumplex model of emotion also used by Spotify to characterise tracks
+HIGH_VALENCE = 0.6
+LOW_VALENCE = 0.4
+HIGH_ENERGY = 0.8
+MID_ENERGY = 0.6
+LOW_ENERGY = 0.4
+HIGH_TEMPO = 140
+HIGH_LOUD = -5
+
+mood_choices = ["Happy","Surprised","Neutral","Sad","Angry"]
+
+conditions = [
+    #defining happy
+    (df['valence'] >= HIGH_VALENCE) & (df['energy'] >= MID_ENERGY),
+    #defining sad
+    (df['valence'] <= LOW_VALENCE) & (df['energy'] <= LOW_ENERGY),
+    #defining angry
+    (df['valence'] <= LOW_VALENCE) & (df['energy'] >= LOW_ENERGY),
+    #defining surprised
+    (df['energy'] >= HIGH_ENERGY) & (df['valence'].between(LOW_VALENCE,HIGH_VALENCE)) & (df['tempo'] >= HIGH_TEMPO) & (df['loudness'] >= HIGH_LOUD),
+    #defining neutral
+    (df['valence'].between(LOW_VALENCE,HIGH_VALENCE)) & (df['energy'].between(LOW_ENERGY,MID_ENERGY))
+]
+
+df['redefined_mood'] = np.select(conditions, mood_choices, default='Unclassified')
+
+#One Hot Encoding (binary)
+X_mood = df[['redefined_mood']]
+ohe = OneHotEncoder(sparse_output=False)
+X_encoded = ohe.fit_transform(X_mood)
+X_encoded_df = pd.DataFrame(X_encoded,columns=ohe.get_feature_names_out(X_mood.columns))
+
+#Scale music numerical features
+Y_features = df.drop(columns=['redefined_mood','track_id','artists','track_name','album_name','explicit','track_genre'])
+scaler_y = StandardScaler()
+y_scaled = scaler_y.fit_transform(Y_features)
+Y_scaled_df = pd.DataFrame(y_scaled, columns=Y_features.columns)
+
+X = X_encoded_df.values.astype(np.float32)
+Y = Y_scaled_df.values.astype(np.float32)
+input_dim = X.shape[1]
+output_dim = Y.shape[1]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+print(f"Input Dimension (Moods): {input_dim}")
+print(f"Output Dimension (Features): {output_dim}")
+
+model = Sequential()
+model.add(Dense(units=32, input_dim=input_dim, activation='relu',name='Input_Mood_Layer'))
+model.add(Dense(units=64, activation='relu', name = 'Hidden_Feature_Mapper'))
+model.add(Dense(units=32, activation = 'relu', name='Hidden_Layer_2'))
+model.add(Dense(units=output_dim, activation='linear',name='Output_Feature_Vector'))
+
+model.compile(optimizer='adam',loss='mse',metrics=['mae'])
+model.summary()
+
+history = model.fit(X_train,Y_train,epochs=50,batch_size=32,validation_split=0.1,verbose=0)
+
+#saving encoder, scaler, model
+joblib.dump(ohe,"models/mood_encoder.pkl")
+joblib.dump(scaler_y,"models/feature_scaler.pkl")
+joblib.save("models/mood_to_features.h5")
+
+
+
+
+
